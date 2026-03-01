01cbce3891a21911833c25ef221c4fa682933fbf
sumon1008 <sumonahmed1008@gmail.com>
2026-03-01 21:29:42 +0600

    start new project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+ 
+
+  return (
+    <>
+
+      <h1>World Wide</h1>
+      <Countries></Countries>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>all countries</h2>
+        </div>
+    );
+};
+
+export default Countries;
