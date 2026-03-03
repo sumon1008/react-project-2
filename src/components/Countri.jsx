486fb94e7ca4af680f5886ebbf44ce6352d6300d
sumon1008 <sumonahmed1008@gmail.com>
2026-03-03 19:38:22 +0600

    new updated

--- src/components/Countri.jsx
+++ src/components/Countri.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Countri = ({countryPromise}) => {
-    console.log(countryPromise.name.common);
+const Countri = ({countryPromise,visitedCountryHandle,visitFlagsHandler}) => {
+
+const [visited, setvisited]=useState(false)
+
+    const visitHandler=()=>{
+
+        setvisited(visited? false:true)
+        visitedCountryHandle(countryPromise)
+    }
+   
     return (
-        <div className='card'>
+        <div className={`card ${visited && 'visited-color'}`}>
             <h2>{countryPromise.name.common}</h2>
             <img src={countryPromise.flags.flags.png} alt="" />
-            
+            <p>Area:{countryPromise.area.area} {countryPromise.area.area>300000? "Big country":"Small country" }</p>
+
+            <button onClick={visitHandler}>{visited? "Visited": "not visited" }</button>
+                <button onClick={()=>{visitFlagsHandler(countryPromise.flags.flags.png)}}>visit flags</button>
         </div>
     );
 };
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,21 +1,47 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Countri from "../Countri";
 
+
 const Countries = ({ allCountries }) => {
+
+const [countrieVisited,setCountriesVisited]=useState([])
+
+const [visitFlags,setVisitFlags]=useState([])
+
+const visitedCountryHandle=(countryPromise)=>{
+
+const newVisitedCountries=[...countrieVisited,countryPromise]
+setCountriesVisited(newVisitedCountries)
+}
+
+const visitFlagsHandler=(flags)=>{
+  const collectedFlags=[...visitFlags,flags]
+  setVisitFlags(collectedFlags)
+}
   const countriesData = use(allCountries);
   const countries = countriesData.countries;
   return (
-    <div> 
-        <h2>All Countries {countries.length}</h2>
-           <div className="contain">
+    <div>
+      <h2>All Countries {countries.length}</h2>
+      <h3>Tottal Visited countries:{countrieVisited.length}</h3>
+      <h3>total flags:{visitFlags.length}</h3>
+      <div>{
+        visitFlags.map(flag=> <img src={flag} alt="" /> )
+        }</div>
+      <ol>
+ {countrieVisited.map(country=><li>{country.name.common}</li>)
+ }
+      </ol>
      
-      {
-      
-      countries.map(country=><Countri countryPromise={country}></Countri>)
-      }
+      <div className="contain">
+        {countries.map((country) => (
+          <Countri key={country.ccn3.ccn3}
+           countryPromise={country}
+           visitFlagsHandler={visitFlagsHandler}
+            visitedCountryHandle={visitedCountryHandle}></Countri>
+        ))}
+      </div>
     </div>
-    </div>
- 
   );
 };
 
